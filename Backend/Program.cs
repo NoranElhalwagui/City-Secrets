761bdff592f0b225c71c7a7980271c651c6546fd
MYAR025 <M.Saad2387@nu.edu.eg>
2025-12-01 06:05:30 +0200

    .

---
+++ Backend/Program.cs
@@ -0,0 +1,20 @@
+﻿using TempBackend.Models;
+
+partial class Program
+{
+    static void Main()
+    {
+        var place = new Place
+        {
+            Name = "Hidden Gem Cafe",
+            Address = "123 Street",
+            Latitude = 30.0,
+            Longitude = 31.0,
+            OpeningHours = "9 AM - 9 PM",
+            AveragePrice = 25.0m,
+            CategoryId = 1
+        };
+
+        Console.WriteLine("Place created: " + place.Name);
+    }
+}
