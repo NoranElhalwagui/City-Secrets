45d42bbe24e39132663f2f48b267105373261e67
Noran <noranelhalwagy515@gmail.com>
2025-12-04 03:53:45 +0200

    CreatedPlaceController

---
+++ CitySecrets/CitySecrets/Controllers/PlaceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitySecrets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaceController : ControllerBase
+    {
+    }
+}
